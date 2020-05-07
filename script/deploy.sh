4250d97f946fa0651785372cd8346c583caa012d
Nikita Prokopov <niki@tonsky.me>
2020-05-08 01:41:19 +0200

    script/revision.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,10 +1,4 @@
 #!/bin/zsh -euo pipefail
-
 cd "`dirname $0`/.."
-
-setopt BASH_REMATCH
-COORD=`git describe --tags --match "*.*.0"`
-if [[ $COORD =~ '([0-9]+.[0-9]+).0-([0-9]+)-[a-z0-9]+' ]] ; then
-    REVISION="${BASH_REMATCH[2]}.${BASH_REMATCH[3]}"
-    mvn -Drevision=$REVISION deploy
-fi
+REVISION=`./script/revision.sh`
+mvn -Drevision=$REVISION deploy
---
+++ script/revision.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh -euo pipefail
+
+cd "`dirname $0`/.."
+
+setopt BASH_REMATCH
+COORD=`git describe --tags --match "*.*.0"`
+if [[ $COORD =~ '([0-9]+.[0-9]+).0-([0-9]+)-[a-z0-9]+' ]] ; then
+    echo "${BASH_REMATCH[2]}.${BASH_REMATCH[3]}"
+fi
