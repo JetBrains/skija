28a425128535556d86077828782b6fb8550e2634
Nikita Prokopov <niki@tonsky.me>
2020-05-26 19:54:51 +0200

    Use jars from ~/.m2/repository in lwjgl example, removed unused lwjgl-assimp, lwjgl-openal, lwjgl-stb

--- examples/lwjgl/script/run.sh
+++ examples/lwjgl/script/run.sh
@@ -3,14 +3,44 @@ cd `dirname $0`/..
 
 mkdir -p target
 
+LWJGL_VER=3.2.3
+LWJGL_OS=macos
+
+LWJGL_LIBS=(
+    lwjgl
+    lwjgl-glfw
+    lwjgl-opengl
+)
+
+HAS_LWJLG=true
+CLASSPATH=target/classes:../../target/classes
+
+for LIB in "${LWJGL_LIBS[@]}"; do
+    JAR=~/.m2/repository/org/lwjgl/$LIB/$LWJGL_VER/$LIB-$LWJGL_VER.jar
+    NATIVE_JAR=~/.m2/repository/org/lwjgl/$LIB/$LWJGL_VER/$LIB-$LWJGL_VER-natives-$LWJGL_OS.jar
+    if [[ ! -f $JAR ]]; then
+        echo "Missing $JAR"
+        HAS_LWJLG=false
+    fi
+    if [[ ! -f $NATIVE_JAR ]]; then
+        echo "Missing $NATIVE_JAR"
+        HAS_LWJLG=false
+    fi
+    CLASSPATH=$CLASSPATH:$JAR:$NATIVE_JAR
+done
+
+if [[ "$HAS_LWJLG" == "false" ]]; then
+    # fetch missing dependencies
+    mvn compile
+fi
+
+
+mkdir -p target/classes
+
 if [ ! -f "target/build_timestamp" ]; then
     touch -t 200912310000 target/build_timestamp
 fi
 
-function join { local IFS="$1"; shift; echo "$*"; }
-CLASSPATH=target/classes:../../target/classes:$(join : `find libs -name "*.jar"`)
-
-mkdir -p target/classes
 find src -name "*.java" -newer target/build_timestamp | xargs javac --release 11 -cp $CLASSPATH -d target/classes
 
 touch target/build_timestamp
