392301705c82a912fc8c1cbe80973a0e8cbf628e
Nikita Prokopov <niki@tonsky.me>
2020-09-22 19:18:38 +0200

    Different artifacts for shared/native

--- script/ci_deploy_macos.sh
+++ script/ci_deploy_macos.sh
@@ -3,5 +3,5 @@ set -o errexit -o nounset -o pipefail
 cd "`dirname $0`/.."
 
 REVISION=$(./script/revision.sh)
-sed -i "" -e "s/[$][{]revision[}]/$REVISION/g" -e "s/[$][{]platform[}]/macos-x64/g" pom.native.xml
+sed -i "" -e "s/[$][{]revision[}]/$REVISION/g" -e "s/[$][{]platform[}]/macos/g" pom.native.xml
 mvn --batch-mode --file pom.native.xml --settings settings.xml -DskipTests -Dspace.username=Nikita.Prokopov -Dspace.password=${SPACE_TOKEN} deploy
--- script/ci_deploy_shared.sh
+++ script/ci_deploy_shared.sh
@@ -3,6 +3,5 @@ set -o errexit -o nounset -o pipefail
 cd "`dirname $0`/.."
 
 REVISION=$(./script/revision.sh)
-# mvn --batch-mode --file pom.shared.xml versions:set -DnewVersion=${REVISION}-shared
 sed -i "s/[$][{]revision[}]/$REVISION/g" pom.shared.xml
 mvn --batch-mode --file pom.shared.xml --settings settings.xml -DskipTests -Dspace.username=Nikita.Prokopov -Dspace.password=${SPACE_TOKEN} deploy
