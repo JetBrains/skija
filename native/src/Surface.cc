c02cf057425939c311924e6040ffc43061ed589c
mzgreen <yairobbe@gmail.com>
2020-11-23 15:29:38 +0100

    Add more Surface bindings (#60)

--- native/src/Surface.cc
+++ native/src/Surface.cc
@@ -2,6 +2,7 @@
 #include <jni.h>
 #include "GrDirectContext.h"
 #include "SkSurface.h"
+#include "interop.hh"
 
 extern "C" JNIEXPORT jlong JNICALL Java_org_jetbrains_skija_Surface__1nMakeFromBackendRenderTarget
   (JNIEnv* env, jclass jclass, jlong pContext, jlong pBackendRenderTarget, jint surfaceOrigin, jint colorType, jlong colorSpacePtr) {
@@ -41,8 +42,116 @@ extern "C" JNIEXPORT jlong JNICALL Java_org_jetbrains_skija_Surface__1nGetCanvas
     return reinterpret_cast<jlong>(surface->getCanvas());
 }
 
+extern "C" JNIEXPORT jint JNICALL Java_org_jetbrains_skija_Surface__1nGetWidth
+  (JNIEnv* env, jclass jclass, jlong ptr) {
+    SkSurface* surface = reinterpret_cast<SkSurface*>(static_cast<uintptr_t>(ptr));
+    return surface->width();
+}
+
+extern "C" JNIEXPORT jint JNICALL Java_org_jetbrains_skija_Surface__1nGetHeight
+  (JNIEnv* env, jclass jclass, jlong ptr) {
+    SkSurface* surface = reinterpret_cast<SkSurface*>(static_cast<uintptr_t>(ptr));
+    return surface->height();
+}
+
 extern "C" JNIEXPORT jlong JNICALL Java_org_jetbrains_skija_Surface__1nMakeImageSnapshot
   (JNIEnv* env, jclass jclass, jlong ptr) {
     SkSurface* surface = reinterpret_cast<SkSurface*>(static_cast<uintptr_t>(ptr));
     return reinterpret_cast<jlong>(surface->makeImageSnapshot().release());
+}
+
+extern "C" JNIEXPORT jlong JNICALL Java_org_jetbrains_skija_Surface__1nMakeImageSnapshotR
+  (JNIEnv* env, jclass jclass, jlong ptr, jint left, jint top, jint right, jint bottom) {
+    SkSurface* surface = reinterpret_cast<SkSurface*>(static_cast<uintptr_t>(ptr));
+    return reinterpret_cast<jlong>(surface->makeImageSnapshot({left, top, right, bottom}).release());
+}
+
+extern "C" JNIEXPORT jint JNICALL Java_org_jetbrains_skija_Surface__1nGenerationId
+  (JNIEnv* env, jclass jclass, jlong ptr) {
+    SkSurface* surface = reinterpret_cast<SkSurface*>(static_cast<uintptr_t>(ptr));
+    return surface->generationID();
+}
+
+extern "C" JNIEXPORT jboolean JNICALL Java_org_jetbrains_skija_Surface__1nReadPixels
+  (JNIEnv* env, jclass jclass, jlong ptr, jlong bitmapPtr, jint srcX, jint srcY) {
+    SkSurface* surface = reinterpret_cast<SkSurface*>(static_cast<uintptr_t>(ptr));
+    SkBitmap* bitmap = reinterpret_cast<SkBitmap*>(static_cast<uintptr_t>(bitmapPtr));
+    return surface->readPixels(*bitmap, srcX, srcY);
+}
+
+extern "C" JNIEXPORT void JNICALL Java_org_jetbrains_skija_Surface__1nWritePixels
+  (JNIEnv* env, jclass jclass, jlong ptr, jlong bitmapPtr, jint x, jint y) {
+    SkSurface* surface = reinterpret_cast<SkSurface*>(static_cast<uintptr_t>(ptr));
+    SkBitmap* bitmap = reinterpret_cast<SkBitmap*>(static_cast<uintptr_t>(bitmapPtr));
+    surface->writePixels(*bitmap, x, y);
+}
+
+extern "C" JNIEXPORT void JNICALL Java_org_jetbrains_skija_Surface__1nFlushAndSubmit
+  (JNIEnv* env, jclass jclass, jlong ptr, jboolean syncCpu) {
+    SkSurface* surface = reinterpret_cast<SkSurface*>(static_cast<uintptr_t>(ptr));
+    surface->flushAndSubmit(syncCpu);
+}
+
+extern "C" JNIEXPORT void JNICALL Java_org_jetbrains_skija_Surface__1nFlush
+  (JNIEnv* env, jclass jclass, jlong ptr) {
+    SkSurface* surface = reinterpret_cast<SkSurface*>(static_cast<uintptr_t>(ptr));
+    surface->flush();
+}
+
+extern "C" JNIEXPORT jboolean JNICALL Java_org_jetbrains_skija_Surface__1nUnique
+  (JNIEnv* env, jclass jclass, jlong ptr) {
+    SkSurface* surface = reinterpret_cast<SkSurface*>(static_cast<uintptr_t>(ptr));
+    return surface->unique();
+}
+
+extern "C" JNIEXPORT jobject JNICALL Java_org_jetbrains_skija_Surface__1nGetImageInfo
+  (JNIEnv* env, jclass jclass, jlong ptr) {
+    SkSurface* surface = reinterpret_cast<SkSurface*>(static_cast<uintptr_t>(ptr));
+    const SkImageInfo& info = surface->imageInfo();
+    return env->NewObject(skija::ImageInfo::cls, skija::ImageInfo::ctor,
+        info.width(),
+        info.height(),
+        static_cast<jint>(info.colorType()),
+        static_cast<jint>(info.alphaType()),
+        reinterpret_cast<jlong>(info.refColorSpace().release()));
+}
+
+extern "C" JNIEXPORT jlong JNICALL Java_org_jetbrains_skija_Surface__1nMakeSurface
+  (JNIEnv* env, jclass jclass, jlong ptr, jint width, jint height) {
+    SkSurface* surface = reinterpret_cast<SkSurface*>(static_cast<uintptr_t>(ptr));
+    sk_sp<SkSurface> newSurface = surface->makeSurface(width, height);
+    return reinterpret_cast<jlong>(newSurface.release());
+}
+
+extern "C" JNIEXPORT jlong JNICALL Java_org_jetbrains_skija_Surface__1nMakeSurfaceI
+  (JNIEnv* env, jclass jclass, jlong ptr, jint width, jint height, jint colorType, jint alphaType, jlong colorSpacePtr) {
+    SkSurface* surface = reinterpret_cast<SkSurface*>(static_cast<uintptr_t>(ptr));
+    SkColorSpace* colorSpace = reinterpret_cast<SkColorSpace*>(static_cast<uintptr_t>(colorSpacePtr));
+    SkImageInfo imageInfo = SkImageInfo::Make(width,
+                                              height,
+                                              static_cast<SkColorType>(colorType),
+                                              static_cast<SkAlphaType>(alphaType),
+                                              sk_ref_sp<SkColorSpace>(colorSpace));
+    sk_sp<SkSurface> newSurface = surface->makeSurface(imageInfo);
+    return reinterpret_cast<jlong>(newSurface.release());
+}
+
+extern "C" JNIEXPORT void JNICALL Java_org_jetbrains_skija_Surface__1nDraw
+  (JNIEnv* env, jclass jclass, jlong ptr, jlong canvasPtr, jfloat x, jfloat y, jlong paintPtr) {
+    SkSurface* surface = reinterpret_cast<SkSurface*>(static_cast<uintptr_t>(ptr));
+    SkCanvas* canvas = reinterpret_cast<SkCanvas*>(static_cast<uintptr_t>(canvasPtr));
+    SkPaint* paint = reinterpret_cast<SkPaint*>(static_cast<uintptr_t>(paintPtr));
+    surface->draw(canvas, x, y, paint);
+}
+
+extern "C" JNIEXPORT void JNICALL Java_org_jetbrains_skija_Surface__1nNotifyContentWillChange
+  (JNIEnv* env, jclass jclass, jlong ptr, jint mode) {
+    SkSurface* surface = reinterpret_cast<SkSurface*>(static_cast<uintptr_t>(ptr));
+    surface->notifyContentWillChange(static_cast<SkSurface::ContentChangeMode>(mode));
+}
+
+extern "C" JNIEXPORT jlong JNICALL Java_org_jetbrains_skija_Surface__1nGetRecordingContext
+  (JNIEnv* env, jclass jclass, jlong ptr) {
+    SkSurface* surface = reinterpret_cast<SkSurface*>(static_cast<uintptr_t>(ptr));
+    return reinterpret_cast<jlong>(surface->recordingContext());
 }
