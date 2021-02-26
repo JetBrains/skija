0e09e6c864548f6be5f5ebc569552ede09893c6b
Nikita Prokopov <niki@tonsky.me>
2021-02-26 01:14:52 +0100

    Sine text decoration

--- examples/scenes/src/TextLineDecorationsScene.java
+++ examples/scenes/src/TextLineDecorationsScene.java
@@ -6,6 +6,8 @@ import org.jetbrains.skija.*;
 import org.jetbrains.skija.shaper.*;
 
 public class TextLineDecorationsScene extends Scene {
+    public final String descenders = " (go-pay)";
+
     public TextLineDecorationsScene() {
         _variants = new String[] { "Scaled", "Identity" };
     }
@@ -18,7 +20,6 @@ public class TextLineDecorationsScene extends Scene {
     @Override
     public void draw(Canvas canvas, int width, int height, float dpi, int xpos, int ypos) {
         canvas.translate(20, 40);
-        String descenders = " (go-pay)";
         float dx = (width - 40) / 8;
 
         for (float w = 1f; w <= 10f; ++w) {
@@ -52,18 +53,18 @@ public class TextLineDecorationsScene extends Scene {
                 canvas.translate(dx, 0);
             }
 
-            try (var line = TextLine.make("Aligned " + w + "px" + descenders, inter13);) {
-                var intervals = (float) Math.floor(line.getWidth() / (w * 2f));
-                var interval = line.getWidth() / (intervals + 0.5f);
-                try (var circle = new Path().addCircle(w / 2f, 0, w / 2f);
-                     var effect = PathEffect.makePath1D(circle, interval, 0, PathEffect.Style.TRANSLATE);
-                     var stroke = new Paint().setColor(0xFFCC3333).setPathEffect(effect);)
-                {
-                    canvas.drawLine(0, 1f + w / 2f, (float) Math.floor(line.getWidth()), 1f + w / 2f, stroke);
-                    canvas.drawTextLine(line, 0, 0, blackFill);
-                    canvas.translate(dx, 0);
-                }
-            }
+            // try (var line = TextLine.make("Aligned " + w + "px" + descenders, inter13);) {
+            //     var intervals = (float) Math.floor(line.getWidth() / (w * 2f));
+            //     var interval = line.getWidth() / (intervals + 0.5f);
+            //     try (var circle = new Path().addCircle(w / 2f, 0, w / 2f);
+            //          var effect = PathEffect.makePath1D(circle, interval, 0, PathEffect.Style.TRANSLATE);
+            //          var stroke = new Paint().setColor(0xFFCC3333).setPathEffect(effect);)
+            //     {
+            //         canvas.drawLine(0, 1f + w / 2f, (float) Math.floor(line.getWidth()), 1f + w / 2f, stroke);
+            //         canvas.drawTextLine(line, 0, 0, blackFill);
+            //         canvas.translate(dx, 0);
+            //     }
+            // }
 
             try (var line = TextLine.make("Dashed " + w + "px" + descenders, inter13);
                  var effect = PathEffect.makeDash(new float[] { w * 3, w * 3 }, 0);
@@ -74,18 +75,18 @@ public class TextLineDecorationsScene extends Scene {
                 canvas.translate(dx, 0);
             }
 
-            try (var line = TextLine.make("Aligned " + w + "px" + descenders, inter13);) {
-                var dashes = (float) Math.floor(line.getWidth() / (w * 6f));
-                var dashWidth = line.getWidth() / (dashes + 0.5f) / 2f;
+            // try (var line = TextLine.make("Aligned " + w + "px" + descenders, inter13);) {
+            //     var dashes = (float) Math.floor(line.getWidth() / (w * 6f));
+            //     var dashWidth = line.getWidth() / (dashes + 0.5f) / 2f;
 
-                try (var effect = PathEffect.makeDash(new float[] { dashWidth, dashWidth }, 0);
-                     var stroke = new Paint().setColor(0xFFCC3333).setMode(PaintMode.STROKE).setStrokeWidth(w).setPathEffect(effect);)
-                {
-                    canvas.drawLine(0, 1f + w / 2f, (float) Math.floor(line.getWidth()), 1f + w / 2f, stroke);
-                    canvas.drawTextLine(line, 0, 0, blackFill);
-                    canvas.translate(dx, 0);
-                }
-            }
+            //     try (var effect = PathEffect.makeDash(new float[] { dashWidth, dashWidth }, 0);
+            //          var stroke = new Paint().setColor(0xFFCC3333).setMode(PaintMode.STROKE).setStrokeWidth(w).setPathEffect(effect);)
+            //     {
+            //         canvas.drawLine(0, 1f + w / 2f, (float) Math.floor(line.getWidth()), 1f + w / 2f, stroke);
+            //         canvas.drawTextLine(line, 0, 0, blackFill);
+            //         canvas.translate(dx, 0);
+            //     }
+            // }
 
             try (var line = TextLine.make("Wavy " + w + "px" + descenders, inter13);
                  var element = new Path().moveTo(0, -1.5f).lineTo(2, 0.5f).lineTo(4, -1.5f).lineTo(4, -0.5f).lineTo(2, 1.5f).lineTo(0, -0.5f).closePath().transform(Matrix33.makeScale(w));
@@ -100,6 +101,8 @@ public class TextLineDecorationsScene extends Scene {
                 canvas.translate(dx, 0);
             }
 
+            drawSine(canvas, w, dx, dpi);
+
             try (var line = TextLine.make("Striked " + w + "px" + descenders, inter13);
                  var stroke = new Paint().setColor(0xFFCC3333).setMode(PaintMode.STROKE).setStrokeWidth(w);)
             {
@@ -112,6 +115,37 @@ public class TextLineDecorationsScene extends Scene {
 
             canvas.restore();
             canvas.translate(0, 30);
+        }
+    }
+
+    public void drawSine(Canvas canvas, float w, float dx, float dpi) {
+        var strokew = w * dpi;
+        var texw = strokew * 4f;
+        var texh = strokew * 2.5f;
+        var control = strokew * 1f;
+        
+        try (var line = TextLine.make("Sinus " + w + "px" + descenders, inter13);
+             var surface = Surface.makeRasterN32Premul((int) Math.ceil(texw), (int) Math.ceil(texh));
+             var path = new Path()
+                            .moveTo(0, texh - 0.5f * strokew)
+                            .cubicTo(control, texh - 0.5f * strokew, texw / 2f - control, 0.5f * strokew, texw / 2f, 0.5f * strokew)
+                            .cubicTo(texw / 2 + control, 0.5f * strokew, texw - control, texh - 0.5f * strokew, texw, texh - 0.5f * strokew);
+             var stroke = new Paint().setColor(0xFFCC3333).setMode(PaintMode.STROKE).setStrokeWidth(strokew);)
+        {
+            var c = surface.getCanvas();
+            c.drawPath(path, stroke);
+            try (var shader = surface.makeImageSnapshot().makeShader(FilterTileMode.REPEAT, FilterTileMode.REPEAT);
+                 var texture = new Paint().setShader(shader);)
+            {
+                canvas.save();
+                canvas.translate(0, 1);
+                canvas.scale(1 / dpi, 1 / dpi);
+                canvas.drawRect(Rect.makeXYWH(0, 0, (float) Math.floor(line.getWidth()) * dpi, texh), texture);
+                canvas.restore();
+
+                canvas.drawTextLine(line, 0, 0, blackFill);
+                canvas.translate(dx, 0);
+            }
         }
     }
 }
